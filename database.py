abea0cd9b969a1790a8f7b1cd008f450ee398d9f
Daniyil Preen <daniyil.aurora@gmail.com>
2025-09-21 21:52:16 +0200

    add/remove methods were added

--- database.py
+++ database.py
@@ -1,4 +1,5 @@
 import sqlite3
+from datetime import datetime
 
 class Database():
     def __init__(self) -> None:
@@ -12,7 +13,15 @@ class Database():
         if not table:
             self.__create_table()
 
+    def __add_record(self, amount: float, description: str, category: str, date: datetime) -> None:
+        self.cursor.execute("INSERT INTO finances(amount, description, category, date) VALUES (?, ?, ?, ?);", (amount, description, category, date))
+        self.connection.commit()
+
+    def __delete_record(self, id: int):
+        self.cursor.execute("DELETE FROM finances WHERE id=?;", (id,))
+        self.connection.commit()
+
     def __create_table(self) -> None:
-        self.cursor.execute("CREATE TABLE finances(amount real, description varchar(255), category varchar(255), date datetime)")
+        self.cursor.execute("CREATE TABLE finances(id integer primary key, amount real, description varchar(255), category varchar(255), date datetime);")
     def __delete_table(self) -> None:
-        self.cursor.execute("DROP TABLE finances")
+        self.cursor.execute("DROP TABLE finances;")
