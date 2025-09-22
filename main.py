44c1d19f19474179bd6e1261fce84abf84317a8e
Daniyil Preen <daniyil.aurora@gmail.com>
2025-09-22 17:51:07 +0200

    Adding basic flask

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+from flask import Flask, render_template
 from database import Database
 
 database = Database()
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template("index.html", name="test")
+
+if __name__ == "__main__":
+    app.run(debug=True)
